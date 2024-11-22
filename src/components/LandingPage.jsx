8c64c100ec99cc3578bf598c8a5471b746d543a7
Esteban Ruiz <ed.esteban.ruiz@gmail.com>
2024-11-22 02:40:12 -0500

    Dont know what i'm doing

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -127,6 +127,10 @@ const Personas = () => {
       if (isEditMode && (field === 'password' || field === 'passwordConfirmation')) {
         return;
       }
+
+      if (isEditMode && data[field] === "") {
+        return;
+      }
       
       const error = validateField(field, data[field]);
       if (error) {
@@ -676,7 +680,9 @@ const Personas = () => {
           </Modal.Body>
           <Modal.Footer>
             <div className="flex justify-end space-x-4">
-              <Button className="bg-red-500 hover:bg-red-800" onClick={closeModal} disabled={isLoading}>
+              <Button className="bg-red-500 hover:bg-red-800" 
+                onClick={closeModal} 
+                disabled={isLoading}>
                 Cancelar
               </Button>
               <Button 
@@ -870,17 +876,33 @@ const Personas = () => {
               <Button
                 className="bg-red-500 hover:bg-red-800"
                 onClick={closeModalEdit}
+                disabled={isLoading}
               >
                 Cancelar
               </Button>
               <Button
-                className="bg-[#7747FF] hover:bg-[#5f35d5]"
+                className={`inline-block py-2 px-6 rounded-l-xl rounded-t-xl ${
+                    isLoading 
+                      ? 'bg-[#9f7fff] cursor-not-allowed' 
+                      : 'bg-[#7747FF] hover:bg-white hover:text-[#7747FF] focus:text-[#7747FF] focus:bg-gray-200'
+                  } text-gray-50 font-bold leading-loose transition duration-200`}
                 onClick={() => {
                   handleEditUser(actualId);
                   closeModalEdit();
                 }}
+                disabled={isLoading}
               >
-                Editar
+                {isLoading ? (
+                  <div className="flex items-center">
+                    <svg className="animate-spin h-5 w-5 mr-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                      <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+                      <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                    </svg>
+                    Procesando...
+                  </div>
+                ) : (
+                  "Editar"
+                )}
               </Button>
             </div>
           </Modal.Footer>
